c3c76cb7819668ef5bbc7cde07d8534994dd7db7
shivank dixit <shivankdev@gmail.com>
2022-07-07 16:06:11 +0530

    find city astrologers section set talk and chat page

---
+++ src/app/components/find-city-astrologers/find-city-astrologers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-find-city-astrologers',
+  templateUrl: './find-city-astrologers.component.html',
+  styleUrls: ['./find-city-astrologers.component.scss']
+})
+export class FindCityAstrologersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
