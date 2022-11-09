d1f826af22b06fd770775c58f20418964b37448a
ogm2022 <msaliomg2022@gmail.com>
2022-11-09 18:16:35 +0530

    done

--- src/app/config/baseurl.service.ts
+++ src/app/config/baseurl.service.ts
@@ -7,8 +7,8 @@ import { Injectable } from '@angular/core';
 
 export class BaseurlService {
 
-  //  BASE_URL = 'https://www.onegodmed.com/Web-api/';
-  BASE_URL = 'http://localhost/new_ogm/Web-api/';
+    BASE_URL = 'https://www.onegodmed.com/Web-api/';
+  //BASE_URL = 'http://localhost/new_ogm/Web-api/';
 
   constructor() { }
 
